2fc0b34a2e67479c4f11ad1a8bf487ca3a079543
mech071 <suprit2snehasis@gmail.com>
2025-10-24 19:20:56 +0530

    added mobile support

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const container = document.querySelector("#container");
 
-function color() {
+function color(element) {
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
-    this.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    element.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
 }
 let n = 16;
 function grid(n) {
@@ -12,29 +12,35 @@ function grid(n) {
     for (let i = 0; i < n * n; i++) {
         const element = document.createElement("div");
         element.classList.add('box');
-        element.style.width = 480 / n + "px";
-        element.style.height = 480 / n + "px";
-        element.addEventListener("mouseover", color);
+        let containerWidth = container.clientWidth;
+        element.style.width = containerWidth / n + "px";
+        element.style.height = containerWidth / n + "px";
+        element.addEventListener("mouseover", function () {
+            color(element);
+        });
+        element.addEventListener("touchstart", function () {
+            color(element);
+        });
+        element.addEventListener("touchmove", function (e) {
+            e.preventDefault();
+            color(element);
+        });
         container.append(element);
     }
 }
 grid(n);
 function changeSize() {
-    let isValid = false;
-    while (!isValid) {
-        let newSize = prompt("Enter new grid size (between 2 - 100)");
-        if (newSize >= 2 && newSize <= 100) {
-            isValid = true;
-            n = newSize;
-            grid(n);
-        } else {
-            alert('Invalid input! Number should be between 2 and 100:');
-        }
+    let newSize = prompt("Enter new grid size (between 2 - 100)");
+    if (newSize >= 2 && newSize <= 100) {
+        n = newSize;
+        grid(n);
+    } else {
+        alert('Invalid input! Number should be between 2 and 100:');
     }
 }
 const button = document.querySelector(".btn-grid")
 button.addEventListener("click", changeSize)
 const reset = document.querySelector(".btn-reset");
 reset.addEventListener('click', function () {
-  grid(16);
+    grid(16);
 });
